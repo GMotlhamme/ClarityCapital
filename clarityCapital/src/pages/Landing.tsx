3c390221da7b96d2851753595e6a3c4a9ecb38f3
Gomolemo Motlhamme <gotelltaylor@gmail.com>
2026-04-22 11:20:50 +0200

    Refactor Landing page styles for improved readability and consistency

--- clarityCapital/src/pages/Landing.tsx
+++ clarityCapital/src/pages/Landing.tsx
@@ -8,15 +8,15 @@ export default function Landing(){
             <div className="flex flex-col justify-center text-center gap-8 p-8">
 
                 {/* <h2 className="text-7xl leading-20">Banking Simplified</h2> */}
-                <p className="text-2xl">Experience transparent, efficient, and secure financial management</p>
+                <p className="text-2xl text-blue-950">Experience transparent, efficient, and secure financial management.</p>
                 <div className="flex flex-row w-full gap-4">
 
-                <Link className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full text-white text-start py-4 px-4 text-xl rounded" to={"/Register"}>Sign Up</Link>
-                <Link className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full text-white text-start py-4 px-4 text-xl rounded" to={"/Login"}>Log in</Link>
+                <Link className="border-2 border-blue-600 hover:bg-blue-700 cursor-pointer w-full text-start py-4 px-4 text-xl rounded" to={"/Register"}>Sign Up</Link>
+                <Link className="border-2 border-blue-600 hover:bg-blue-700 cursor-pointer w-full text-start py-4 px-4 text-xl rounded" to={"/Login"}>Log in</Link>
                 </div>
             </div>
 
-            <section className="flex p-8 gap-4">
+            <section className="flex text-blue-950 mt-12 p-8 gap-4">
                 <div className="flex flex-col gap-2 text-center">
                    <i className="bi bi-globe text-4xl"></i> 
                    <p>Global SWIFT Transfers</p>
