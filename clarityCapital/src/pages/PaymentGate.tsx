a013d3d384e510dd47145be9ad2fa99a7c7e2c47
Gomolemo Motlhamme <gotelltaylor@gmail.com>
2026-04-22 12:56:16 +0200

    Refactor PaymentGate component for improved layout and user experience

--- clarityCapital/src/pages/PaymentGate.tsx
+++ clarityCapital/src/pages/PaymentGate.tsx
@@ -1,40 +1,47 @@
-export default function PaymentGate(){
-    return(
+export default function PaymentGate() {
+    return (
         <>
-        <section>
-            <form action="">
-                <div>
-                    <label htmlFor="">Amount</label>
-                    <input required type="text" />
-                </div>
-                <div>
-                    <label htmlFor="">Currency</label>
-                    <select required name="" id=""></select>
-                </div>
-                <div>
-                    <label htmlFor="">Provider</label>
-                    <input readOnly type="radio" />
-                </div>
-                <button>Continue</button>
-            </form>
+            <section className="flex flex-col gap-8 p-8">
+                <form className="border-2 border-neutral-700 p-8 rounded" action="">
+                    <div className="flex flex-col gap-2 mb-4">
+                        <label htmlFor="">Amount</label>
+                        <input className="p-4 border-2 border-neutral-700 rounded" required type="number" />
+                    </div>
+                    <div className="flex flex-col gap-2 mb-4">
+                        <label htmlFor="">Currency</label>
+                        <select className="p-4 border-2 border-neutral-700 rounded w-full" required name="" id="">
+                            <option value="">Select Currency</option>
+                            <option value="USD">USD</option>
+                            <option value="EUR">EUR</option>
+                        </select>
+                    </div>
+                    <div className="flex flex-col  gap-2 mb-4">
+                        <label htmlFor="">Provider</label>
+                        <div className="items-center flex gap-2">
+                            <input className="accent-blue-500" readOnly type="radio" checked/>
+                            <label htmlFor="">SWIFT</label>
+                        </div>
+                    </div>
+                    <button className="flex justify-center w-full border-2 border-blue-800 text-blue-950 rounded p-4 cursor-pointer">Continue</button>
+                </form>
 
-            <section>
-            <h3>Finalization</h3>
-            <form action="">
-                <div>
-                    <label htmlFor="">Payee Account Information</label>
-                    <input required type="text" />
-                </div>
-                <div>
-                    <label htmlFor="">Swift code</label>
-                    <select required name="" id=""></select>
-                </div>
-                <button>Pay Now</button>
-            </form>
+                <section className="border-2 border-neutral-700 p-8 rounded">
+                    <h3 className="text-xl">Finalization</h3>
+                    <form action="">
+                        <div className="flex flex-col gap-2 mb-4">
+                            <label htmlFor="">Payee Account Information</label>
+                            <input className="p-4 border-2 border-neutral-700 rounded" required type="text" />
+                        </div>
+                        <div className="flex flex-col gap-2 mb-4">
+                            <label htmlFor="">Swift code</label>
+                            <input className="p-4 border-2 border-neutral-700 rounded" type="number" required />
+                        </div>
+                        <button className="flex justify-center w-full border-2 border-blue-800 text-blue-950 rounded p-4 cursor-pointer">Pay Now</button>
+                    </form>
+                </section>
+
+
             </section>
-
-            
-        </section>
         </>
     )
 }
